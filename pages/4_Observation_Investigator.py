b2e02898aaee3649afd3d1ef2796fb064c0c1fb3
Akshay Dharmavaram <30520349+Aks-Dmv@users.noreply.github.com>
2024-09-24 18:52:24 -0400

    Update 4_Observation_Investigator.py

--- pages/4_Observation_Investigator.py
+++ pages/4_Observation_Investigator.py
@@ -8,7 +8,7 @@ from utils.google_sheet_utils import create_new_chat_sheet, get_case_description
 from utils.llm_utils import create_llm, get_prompt
 from utils.page_formatting import add_investigator_formatting
 from utils.initialize_session import initialize_investigator_session
-from utils.chatbot_utils import fetch_similar_data, update_session
+from utils.chatbot_utils import fetch_similar_data, update_session, fetch_real_time_gsheets_data
 
 check_if_already_logged_in()
 add_investigator_formatting()
@@ -21,7 +21,7 @@ observation_chat_chain = get_prompt() | llm | StrOutputParser()
 if prompt := st.chat_input("What would you like to ask?"):
 
     with get_openai_callback() as cb:
-        new_output = observation_chat_chain.invoke(fetch_similar_data(prompt),)
+        new_output = observation_chat_chain.invoke(fetch_real_time_gsheets_data(prompt),)
 
     update_session(new_output)
 
